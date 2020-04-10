f77c1a0319b874853310a34a10086ff7108cab73
Chris Moe <chris.moe@gmail.com>
2020-04-10 09:37:36 -0700

    Got navigation links created dynamically as well as highlighting active page

--- build.py
+++ build.py
@@ -1,66 +1,84 @@
 import glob
 import os
+from jinja2 import Template
+import datetime
 
-pages = []
+pages = [] # creating an empty pages list to fill dynamically fill from the contents of the content folder
 
-def page_list():
-    all_html_files = glob.glob('content/*.html')
+def year():
+    now = datetime.datetime.now()
+    return str(now.year)
+
+def my_pages(): #this function creates a dynamically generated list of pages contained in the content folder
+    all_html_files = glob.glob('content/*.html') # list of all files inside the content folder
     for item in all_html_files:
-        file_path = os.path.basename(item)
-        name_only, extension = os.path.splitext(file_path)
-        pages.append({
-            "filename": item,
+        file_path = os.path.basename(item) #prints the filepath, or in this case, the filename since all files are inside the same folder
+        name_only, extension = os.path.splitext(file_path) #this splits the value of file_path, which is something like index.html into two parts, name_only, which returns index and extenstion which returns .html
+        pages.append({ # this appeads the following key/value pairs to my currently empty pages list
+            "filename": item, 
             "output": 'docs/' + file_path,
-            "active": "{{active_" + name_only + "}}",
+            "active": 'active',
             "title": name_only,
             "link": file_path,
         })
 
-
-page_list()
-print('-----------------------------------')
+my_pages()
 
 from pprint import pprint
 pprint(pages)
 
-# print(pages)
 
-def year():
-    import datetime
-    now = datetime.datetime.now()
-    return str(now.year)
+for page in pages: # for loop using the pages list that was dynamically generated above
+    page_html = open(page['filename']).read() # defining the content of the page, what will replace {{ contnet }} in the template
+    template_html = open("templates/base.html").read() # defining our template html, basically this is simply saying that template_html is the same as base.html in the templates folder
+    template = Template(template_html) # defining Jinja template... I think...
+    result = template.render({  # rendering the jinja template and assigning the result to the result variable
+        'title': page['title'], # the key title is given the value of title key from the pages list
+        'content': page_html, # the key content is given the value of page_html 
+        'link': page['link'], # the key link is given the value of the link key from the pages list
+        'active': page['active'], # the key active is given the value of the active key from the pages list
+        "year": year(), # grabs current year for copyright
+        'pages':pages,
+    })
+    print(page['link'])
+    print(page['title'])
+    print(page)
 
-def read_in_page(page):
-    template = open("./templates/base.html").read()
-    filename = page['filename']
-    output = page['output']
-    title = page['title']
-    active = page['active']
-    contents = open(filename).read()
-    return template, filename, output, title, contents, active
 
-def add_title_content(template,filename,output,title,active,year):
-        the_title_added = template.replace("{{title}}", title) 
-        filename_in = filename[9:]
-        nav_text = '<a class="nav-link" data-toggle="tab" href=".'+filename_in+'">'
-        year = year()
-        highlighted_nav = the_title_added.replace(nav_text, active).replace("{{year}}", year)
-        if nav_text in template:
-            open(output, "w+").write(highlighted_nav)
-        else:
-            pass
-        contents = open(filename).read()
-        finished_combined_page = highlighted_nav.replace("{{content}}", contents)
-        open(output, "w+").write(finished_combined_page)
-        return finished_combined_page
+    open(page['output'], "w+").write(result) # opens the individual html pages and writes the value of result to the individual pages
+
+
+# def read_in_page(page):
+#     template = open("./templates/base.html").read()
+#     filename = page['filename']
+#     output = page['output']
+#     title = page['title']
+#     active = page['active']
+#     contents = open(filename).read()
+#     return template, filename, output, title, contents, active
+
+# def add_title_content(template,filename,output,title,active,year):
+#         the_title_added = template.replace("{{title}}", title) 
+#         filename_in = filename[9:]
+#         nav_text = '<a class="nav-link" data-toggle="tab" href=".'+filename_in+'">'
+#         year = year()
+#         highlighted_nav = the_title_added.replace(nav_text, active).replace("{{year}}", year)
+#         if nav_text in template:
+#             open(output, "w+").write(highlighted_nav)
+#         else:
+#             pass
+#         contents = open(filename).read()
+#         finished_combined_page = highlighted_nav.replace("{{content}}", contents)
+#         open(output, "w+").write(finished_combined_page)
+#         return finished_combined_page
   
 
-def main():
-    print('rebuilding pages')
-    for page in pages:
-        template, filename, output, title, contents, active = read_in_page(page)
-        add_title_content(template,filename,output,title,active,year)
-        print(output)
-    print('rebuild complete')
-main()
+# def main():
+#     print('rebuilding pages')
+#     for page in pages:
+#         template, filename, output, title, contents, active = read_in_page(page)
+#         add_title_content(template,filename,output,title,active,year)
+#         print(output)
+#     print('rebuild complete')
+# main()
 
